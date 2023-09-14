62fe158618fb655ab887e8c62ddef04033b45bd2
sydangus <131490515+sydangus@users.noreply.github.com>
2023-09-14 15:12:16 -0400

    Add files via upload

---
+++ ExamGame.java
@@ -0,0 +1,13 @@
+public class ExamGame {
+
+   public static void main(String[] args) {
+   
+   System.out.println("Welcome to Terror of 247!");
+   System.out.println("You have three floors to pass through, each floor holding an exam");
+   System.out.println("Pass the exams, go down a floor. If you fail, RESTART THE CLASS!");
+   // Just filler stuff, serves as a base for branching our code from here 
+   
+   
+   }
+
+}
